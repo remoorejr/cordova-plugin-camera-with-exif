82dee861450370986ab7124afa5b35ccbe40717e
Michael Oki <mikeoki.oki@gmail.com>
2018-02-20 11:34:04 +0200

    Updated CordovaUri.java
    Fixed uri issues on Android Oreo

--- src/android/CordovaUri.java
+++ src/android/CordovaUri.java
@@ -27,7 +27,7 @@ import android.support.v4.content.FileProvider;
 import java.io.File;
 
 /*
- * This class exists because Andorid FilesProvider doesn't work on Android 4.4.4 and below and throws
+ * This class exists because Android FilesProvider doesn't work on Android 4.4.4 and below and throws
  * weird errors.  I'm not sure why writing to shared cache directories is somehow verboten, but it is
  * and this error is irritating for a Compatibility library to have.
  *
@@ -50,7 +50,7 @@ public class CordovaUri {
         {
             androidUri = inputUri;
             fileName = getFileNameFromUri(androidUri);
-            fileUri = Uri.parse("content://" + fileName);
+            fileUri = Uri.parse("file://" + fileName);
         }
         else
         {
